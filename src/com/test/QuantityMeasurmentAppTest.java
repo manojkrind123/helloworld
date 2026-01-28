2da4a555b9030b9f6f4da91c4c90c96a6a0f5252
MA20344154 <manoj.kumar319@wipro.com>
2026-01-28 16:22:23 +0530

    UC1 feet measurment equality test cases

---
+++ src/com/test/QuantityMeasurmentAppTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import com.QuantityMeasurmentApp.Feet;
+
+public class QuantityMeasurmentAppTest {
+
+
+    public void testFeetEquality_SameValue(){
+
+       Feet f1 = new Feet(1.0);
+
+    }
+}
